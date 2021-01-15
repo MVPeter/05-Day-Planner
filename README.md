33e41a44bb675a167bf67b8d480743c48423b556
MattV <mvpetersen1@gmail.com>
2021-01-15 13:52:51 -0700

    updated readme, js hour formats, colum heights widths.

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # 05-Day-Planner
 Business hour task manager website.
 
+https://mvpeter.github.io/05-Day-Planner/.
--- assets/scripts.js
+++ assets/scripts.js
@@ -2,6 +2,8 @@
 
 // GIVEN I am using a daily planner to create a schedule
 // WHEN I open the planner
+
+//Check local storage and write it back to the page
 function setLocalStorage(i) {
     console.log(i + "      Number text passed to function setlocalStorage")
     var textVal = localStorage.getItem(i);
@@ -14,17 +16,21 @@ function setLocalStorage(i) {
 
 //documennt ready
 $(document).ready(function () {
+    // The current day is displayed at the top of the calendar
+    //current Day, year put at ID currentDay
     var currentDate = moment().format("dddd, MMMM Do");
     $("#currentDay").text(currentDate);
     // var currentHourNum = moment().format("h")
+    
+    //Setup out Hours 9am to 5pm
     for (let i = 9; i < 18; i++) {
 
         //create row
         var rows = $('<div hour="' + i + '" id="' + i + 'row" class="row">');
         //create column for time
-        var timeCol = $('<div class= "col-sm-1"> <div class="hour">' + i + 'am</div>');
+        var timeCol = $('<div class= "col-sm-1"> <div class="hour">' + convert12HourNum(i) + '</div>');
         //create column for text
-        var textCol = $('<div class= "col-sm-9"> <textarea id="' + i + 'text" placeholder="Event description" class="textarea"></textarea>');
+        var textCol = $('<div class= "col-sm-9" m-0> <textarea id="' + i + 'text" placeholder="Event description" class="discription"></textarea>');
         //create column for savebutton
         var saveBtn = $('<div class= "col-sm-2"><button class="saveBtn" id="' + i + '">Save</button>');
 
@@ -40,39 +46,33 @@ $(document).ready(function () {
         setLocalStorage(i);
 
     }
+    
     //store text in localstorage.
-
     $(".saveBtn").click(function () {
-        // for (let i = 9; i < 18; i++) {
-            // console.log(i)
+        
             var buttonNum = $(this).attr('id');
             console.log(buttonNum +"    button ID");
-            // var Numb = '#'
             var localS = '#' + buttonNum + 'text';
-            // console.log(localS);
             var localSVal = $(localS).val();
             console.log(localSVal + "             text Value");
             localStorage.setItem(buttonNum, localSVal);
-        // }
     });
 
-    // THEN the current day is displayed at the top of the calendar
-    //current Day, year put at ID currentDay
-
-    // WHEN I scroll down
-    // THEN I am presented with time blocks for standard business hours
-    //rows for 8am to 5pm
+    //Function to convert 24 hour numbers to 12 hour numbers
+    function convert12HourNum(i){
+        var PMAM = i >=12 ? "PM" : "AM";
+        i = i % 12;
+        i = i ? i : 12;
+        return i + PMAM
+    }
 
     // WHEN I view the time blocks for that day
     // THEN each time block is color-coded to indicate whether it is in the past, present, or future
     //update ID's for past, present, future:  CSS
 
-    // WHEN I click into a time block
-    // THEN I can enter an event
-    //caputer input
 
-    // WHEN I click the save button for that time block
-    // THEN the text for that event is saved in local storage
+
+
     //save buton writes to local storage Day ID.
     //text stays on the document.
 
